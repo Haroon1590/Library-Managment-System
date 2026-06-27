e4821ed40264dcba538271a08da413427ad977fd
Haroon1590 <h999602@gmail.com>
2026-06-27 13:00:38 +0500

    Removed redundancy
    shifted some repeating methods in base class

--- LibraryItems.py
+++ LibraryItems.py
@@ -3,18 +3,11 @@ class LibraryItem:
         self.title=title
         self.author=author
         self.item_id=item_id
+        self.is_available=True
 
 
     def __str__(self):
-        return f"title : {self.title} \nAuthor : {self.author}\nItem_Id : {self.item_id}"
-    
-class Book(LibraryItem):
-    def __init__(self, title, author, item_id, genre, pages):
-        super().__init__(title, author, item_id)
-        self.genre=genre
-        self.pages=pages
-        self.is_available=True
-        
+        return f"title : {self.title}\nAuthor : {self.author}\nItem_Id : {self.item_id}"
     
     def borrow(self):
         if self.is_available:
@@ -22,14 +15,37 @@ class Book(LibraryItem):
         else:
             raise Exception("Item already Borrowed")
     
-    def return_book(self):
+    def return_item(self):
         self.is_available=True
 
     def to_dict(self):
         return self.__dict__
     
+class Book(LibraryItem):
+    def __init__(self, title, author, item_id, genre, pages):
+        super().__init__(title, author, item_id)
+        self.genre=genre
+        self.pages=pages
+        
     def __str__(self):
         return super().__str__()+f"\nGenre : {self.genre}  \nPages : {self.pages} \nAvailabillity : {self.is_available}"
     
+class Magzine(LibraryItem):
+    def __init__(self, title, author, item_id, issue_number, month):
+        super().__init__(title, author, item_id)
+        self.issue_number=issue_number
+        self.month=month
+        
+    def __str__(self):
+        return super().__str__()+f"\nIssue Number : {self.issue_number}\nMonth : {self.month}\nAvailability : {self.is_available}"
 
+class DVD(LibraryItem):
+    def __init__(self, title, author, item_id, duration_mins, director):
+        super().__init__(title, author, item_id)
+        self.duration_mins=duration_mins
+        self.director=director
     
+    def __str__(self):
+        return super().__str__()+f"\nDirector :{self.director}\nDuration : {self.duration_mins}\nAvailabillity : {self.is_available}"
+    
+
